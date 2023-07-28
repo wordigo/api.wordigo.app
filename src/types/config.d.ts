0be37ab38d1a324438961e2bcd62f4198fcb86bc
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2023-07-28 19:39:08 +0300

    fix: swagger problem

--- src/types/config.d.ts
+++ src/types/config.d.ts
@@ -1,15 +1,15 @@
-import { FastifyInstance } from 'fastify';
+import { FastifyInstance } from 'fastify'
 
 declare module 'fastify' {
   interface FastifyInstance {
     config: {
-      NODE_ENV: 'development' | 'production' | 'test';
-      BIND_PORT: number;
-      BIND_ADDR: string;
-      PROJECT_NAME: string;
-      APP_SERVER_NAME: string;
-      DATABASE_URL: string;
-      ENABLE_SWAGGER: boolean;
-    };
+      NODE_ENV: 'development' | 'production' | 'test'
+      BIND_PORT: number
+      BIND_ADDR: string
+      PROJECT_NAME: string
+      APP_SERVER_NAME: string
+      DATABASE_URL: string
+      ENABLE_SWAGGER: string
+    }
   }
 }
