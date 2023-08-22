68113f8a300e52542fa0ff3002c5b2b5770efbb7
sefa kapısız <msefa06@hotmail.com>
2023-08-22 15:19:28 +0300

    feat:language paths detection moved before API

--- apps/api/src/utils/helpers/i18n.helper.ts
+++ apps/api/src/utils/helpers/i18n.helper.ts
@@ -6,6 +6,8 @@ import i18nextMiddleware from 'i18next-http-middleware'
 
 const localesFolder = join(__dirname, '../../../locales')
 
+const pathsOfLanguages: string[] = []
+
 i18next
   .use(i18nextMiddleware.LanguageDetector) // the language detector, will automatically detect the users language, by some criteria... like the query parameter ?lng=en or http header, etc...
   .use(Backend) // you can also use any other i18next backend, like i18next-http-backend or i18next-locize-backend
@@ -14,6 +16,7 @@ i18next
     fallbackLng: 'en',
     preload: readdirSync(localesFolder).filter((fileName) => {
       const joinedPath = join(localesFolder, fileName)
+      pathsOfLanguages.push(fileName.split('.')[0])
       return lstatSync(joinedPath).isDirectory()
     }),
     backend: {
@@ -25,4 +28,4 @@ export const registeringi18nextMiddleware = async (app: any) => {
   app.register(i18nextMiddleware.plugin, { i18next })
 }
 
-export { i18next }
+export { i18next, pathsOfLanguages }
