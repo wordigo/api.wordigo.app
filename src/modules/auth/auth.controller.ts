49f35316a4a79a56a092856f6562f789616a4bcb
osmandlsmn <dlsmnosman@gmail.com>
2023-10-05 18:33:35 +0300

    test

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -61,18 +61,22 @@ export const SignUp = async (req: FastifyRequest<{ Body: SignUpValidationType }>
 
   // await sendMail(email as string, i18next.t("welcome_user"), render(reactEmail));
 
-  await prisma.users.create({
-    data: {
-      avatar_url: `https://wordigo.app/api/dynamic-avatar?username=${username}?size=256`,
-      email,
-      name,
-      username,
-      passwordHash: passwordHashAndSalt.hash,
-      passwordSalt: passwordHashAndSalt.salt,
-      nativeLanguage,
-      provider: Providers.Local,
-    },
-  })
+  try {
+    await prisma.users.create({
+      data: {
+        avatar_url: `https://wordigo.app/api/dynamic-avatar?username=${username}?size=256`,
+        email,
+        name,
+        username,
+        passwordHash: passwordHashAndSalt.hash,
+        passwordSalt: passwordHashAndSalt.salt,
+        nativeLanguage,
+        provider: Providers.Local,
+      }}) 
+  } catch (err) {
+    console.log(err);
+    
+  }
 
   return reply.send(successResult(null, i18next.t(messages.success)))
 }
