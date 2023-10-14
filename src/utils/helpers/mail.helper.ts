390f1c437a704d95f8f7def53fd922b6fc9bc71d
osmandlsmn <dlsmnosman@gmail.com>
2023-10-14 20:25:55 +0300

    feat: add email templates, integrate auth register

--- src/utils/helpers/mail.helper.ts
+++ src/utils/helpers/mail.helper.ts
@@ -1,4 +1,6 @@
-import { SendMailOptions, createTransport } from "nodemailer";
+import fs from 'fs'
+import { SendMailOptions, createTransport } from 'nodemailer'
+import path from 'path'
 
 let transporterConfig = {
   host: process.env.SMTP_MAIL_HOST,
@@ -8,9 +10,9 @@ let transporterConfig = {
     user: process.env.SMPT_MAIL_USER,
     pass: process.env.SMTP_MAIL_PASSWORD,
   },
-};
+}
 
-let transporter = createTransport(transporterConfig);
+let transporter = createTransport(transporterConfig)
 
 export const sendMail = async (to: string, subject: string, html: string) => {
   let mailOptions: SendMailOptions = {
@@ -18,11 +20,22 @@ export const sendMail = async (to: string, subject: string, html: string) => {
     to,
     subject,
     html,
-  };
+  }
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.log(error);
+      console.log(error)
     }
-  });
-};
+  })
+}
+
+export const getMailTemplate = (template: 'welcome' = 'welcome', language: string, replaceVariables: object) => {
+  const templatePath = path.join(__dirname, `../../templates/${template}/${language}.html`)
+  let templateContent = fs.readFileSync(templatePath, 'utf8')
+
+  for (const [key, value] of Object.entries(replaceVariables)) {
+    templateContent = templateContent.replace(new RegExp(`{{${key}}}`, 'g'), value as string)
+  }
+
+  return templateContent
+}
