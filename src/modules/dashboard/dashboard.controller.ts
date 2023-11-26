b9104407b8ecebaa26ba6e84c7d18e336f6301b4
Muhammet Sefa Kapısız <m.sefa06@hotmail.com>
2023-11-26 23:24:45 +0300

    feat: wordInteraction API sceleton created

--- src/modules/dashboard/dashboard.controller.ts
+++ src/modules/dashboard/dashboard.controller.ts
@@ -6,7 +6,7 @@ import { successResult } from '../../utils/constants/results'
 import { prisma } from '@/lib/prisma'
 import { DictionaryInitialTitle } from '../dictionaries/dictionaries.types'
 
-export const GetStatistics = async (req: FastifyRequest, reply: FastifyReply) => {
+export const GeneralStatistic = async (req: FastifyRequest, reply: FastifyReply) => {
   const user = req.user
 
   const dictionaries = await prisma.dictionaries.findMany({ where: { authorId: user.id }, include: { UserWords: true } })
@@ -27,4 +27,18 @@ export const GetStatistics = async (req: FastifyRequest, reply: FastifyReply) =>
   const result = { numberOfDictionaries, numberOfWords, numberOfSubbedDics, numberOfPublicDics }
 
   return reply.send(successResult(result, i18next.t(messages.success)))
+}
+
+export const WordInteraction = async (req: FastifyRequest, reply: FastifyReply) => {
+  const user = req.user
+
+  //const { typeOfStatistic } = req.query
+
+  const userWords = await prisma.userWords.findMany({ where: { authorId: user.id } })
+
+  const firstDate = new Date(Math.min(...userWords.map(u => u.createdDate.getTime())))
+  const day = 30 * firstDate.getMonth() + firstDate.getDay()
+  const week = 4 * firstDate.getMonth() + Math.ceil(firstDate.getDay() / 7)
+
+  //console.log(firstDate, lastDate)
 }
--- src/modules/dashboard/dashboard.schema.ts
+++ src/modules/dashboard/dashboard.schema.ts
@@ -1,7 +1,15 @@
 import { tags } from '../../utils/constants/Tags'
 
-export const GetSchema = {
+export const GeneralStatisticSchema = {
   tags: [tags.Dashboard],
-  summary: 'Wordigo User Dashboard',
+  summary: 'General Statistic of User',
+  description: "Number of Datas User Have",
+  security: [{ JWT: [] }],
+}
+
+export const WordInteractionSchema = {
+  tags: [tags.Dashboard],
+  summary: 'Word Interactions of User',
+  description: "Time Based Statictis of Word Adding/Creating of User",
   security: [{ JWT: [] }],
 }
