b9d33d8842c7bc1d4628e7d66b96bdba6bd2fad1
osmandlsmn <dlsmnosman@gmail.com>
2024-05-19 20:32:26 +0300

    chore: trigger railway

--- prompt/index.ts
+++ prompt/index.ts
@@ -1,73 +1,60 @@
-import { create as createDic } from '../src/modules/dictionaries/dictionaries.service'
-import { create as createWord } from '../src/modules/words/words.service'
 import * as fs from 'fs'
 import { Dictionaries, prisma } from '../src/lib/prisma'
+import { create as createDic } from '../src/modules/dictionaries/dictionaries.service'
+import { create as createWord } from '../src/modules/words/words.service'
 import messages from '../src/utils/constants/messages'
 
 export const promptJsonPath = '/prompt.json'
 
 interface WordObject {
-    text: string
-    translatedText: string
-    nativeLanguage: string
-    targetLanguage: string
+  text: string
+  translatedText: string
+  nativeLanguage: string
+  targetLanguage: string
 }
 
 export interface Prompt {
-    titleOfDic: string,
-    nativeLanguage: string,
-    targetLanguage: string,
-    description: string,
-    level: number,
-    words: WordObject[]
+  titleOfDic: string
+  nativeLanguage: string
+  targetLanguage: string
+  description: string
+  level: number
+  words: WordObject[]
 }
 
 export default async () => {
+  let prompt: Prompt
 
-    let prompt: Prompt
+  fs.readFile(__dirname + promptJsonPath, 'utf8', async (err, data) => {
+    if (err) {
+      console.error('Error reading JSON file:', err)
+      return
+    }
 
-    fs.readFile(__dirname + promptJsonPath, 'utf8', async (err, data) => {
-        if (err) {
-            console.error("Error reading JSON file:", err)
-            return
-        }
+    try {
+      prompt = JSON.parse(data) as Prompt
 
-        try {
-            prompt = JSON.parse(data) as Prompt
+      const user = await prisma.users.findFirst({
+        where: {
+          email: 'user@example.com',
+        },
+      })
 
-            const user = await prisma.users.findFirst(
-                {
-                    where: {
-                        email: 'user@example.com'
-                    }
-                })
+      if (!user) return console.error(messages.user_not_found)
 
-            if (!user)
-                return console.error(messages.user_not_found)
+      const createDicResult = await createDic(prompt.titleOfDic, prompt.targetLanguage, prompt.nativeLanguage, prompt.description, prompt.level, true, user?.id as string)
+      if (!createDicResult.success) return console.error(createDicResult.message)
 
-            const createDicResult = await createDic(
-                prompt.titleOfDic,
-                prompt.targetLanguage,
-                prompt.nativeLanguage,
-                prompt.description,
-                prompt.level,
-                true,
-                user?.id as string
-            )
-            if (!createDicResult.success)
-                return console.error(createDicResult.message)
+      const dictionary = createDicResult.data as Dictionaries
 
-            const dictionary = createDicResult.data as Dictionaries
+      const words = prompt.words
 
-            const words = prompt.words
-
-            for (const word of words) {
-                await createWord(word.text, word.translatedText, word.nativeLanguage, word.targetLanguage, dictionary.id as number, user.id)
-            }
-
-        } catch (err) {
-            console.error("Error parsing JSON data:", err)
-            return
-        }
-    })
+      for (const word of words) {
+        await createWord(word.text, word.translatedText, word.nativeLanguage, word.targetLanguage, dictionary.id as number, user.id, '')
+      }
+    } catch (err) {
+      console.error('Error parsing JSON data:', err)
+      return
+    }
+  })
 }
